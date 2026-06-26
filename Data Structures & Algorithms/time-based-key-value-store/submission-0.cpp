c255ea93762174c2f03e32114d0de433892817a5
01min <thisis01min@gmail.com>
2026-06-26 16:14:12 +0800

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,31 @@
+class TimeMap {
+private:
+    unordered_map<string,vector<pair<int,string>>>db;
+public:
+    TimeMap() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+    }
+    
+    void set(string key, string value, int timestamp) {
+        db[key].push_back({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+        if(db.find(key)==db.end())return "";
+        auto pairs=db[key];
+        int left = 0;
+        int right = pairs.size() - 1;
+        string ans = "";
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (pairs[mid].first <= timestamp) {
+                ans = pairs[mid].second;
+                left = mid + 1;          
+            } else {
+                right = mid - 1; 
+            }
+        }
+        return ans;
+    }
+};
