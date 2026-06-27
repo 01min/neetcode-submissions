967a8a8aa8c20f5050734a9f24535fdb89defa5c
01min <thisis01min@gmail.com>
2026-06-27 10:01:24 +0800

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int maxnum=0;
+        int left=0;
+        int right=0;
+        int index=0;
+        unordered_set<char>st;
+        while(right<s.length()&&left<=right){
+            
+            if(!st.count(s[right])){st.insert(s[right]);index+=1;maxnum=max(maxnum,index);right+=1;}
+            else{st.erase(s[left]);left+=1;index-=1;}
+
+        }
+        return maxnum;
+    }
+
+};
