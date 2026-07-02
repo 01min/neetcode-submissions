946c783069fc4b2c6e6390325063350dc3fb51ef
01min <thisis01min@gmail.com>
2026-07-02 15:21:15 +0800

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        
+        vector<string> result;
+        string current = "";
+        vector<string> mapping = {
+            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        
+        backtrack(digits, 0, mapping, current, result);
+        return result;
+    }
+
+private:
+    void backtrack(const string& digits, int index, const vector<string>& mapping, 
+                   string& current, vector<string>& result) {
+        if (index == digits.length()) {
+            result.push_back(current);
+            return;
+        }
+        int digit = digits[index] - '0';
+        string letters = mapping[digit];
+        for (char c : letters) {
+            current.push_back(c);
+            backtrack(digits, index + 1, mapping, current, result);
+            current.pop_back();
+        }
+    }
+};
