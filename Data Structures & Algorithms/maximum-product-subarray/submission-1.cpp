60e472350d18c792bb1d6fddfba67bced799e658
01min <thisis01min@gmail.com>
2026-07-05 20:10:12 +0800

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int global_max = nums[0];
+        int current_max = nums[0];
+        int current_min = nums[0];
+        
+        for (size_t i = 1; i < nums.size(); ++i) {
+            int num = nums[i];
+            if (num < 0) {
+                swap(current_max, current_min);
+            }
+            current_max = max(num, current_max * num);
+            current_min = min(num, current_min * num);
+            global_max = max(global_max, current_max);
+        }
+        
+        return global_max;
+    }
+};
