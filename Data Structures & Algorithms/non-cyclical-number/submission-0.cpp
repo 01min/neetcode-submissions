f596397323e1c12ca654b0c7b439217b81e4eda4
01min <thisis01min@gmail.com>
2026-07-06 14:48:13 +0800

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+       
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);        
+            fast = getNext(getNext(fast)); 
+        }
+        return fast == 1;
+    }
+
+private:
+    int getNext(int num) {
+        int totalSum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            totalSum += digit * digit;
+            num /= 10;
+        }
+        return totalSum;
+    }
+};
