81a330ebf56a933f072e9f5781cb94607a39d709
01min <thisis01min@gmail.com>
2026-07-07 14:32:53 +0800

    Add: count-squares - submission-1

---
+++ Data Structures & Algorithms/count-squares/submission-1.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+class CountSquares {
+private:
+    int counts[1001][1001];
+    vector<pair<int, int>> unique_points;
+
+public:
+    CountSquares() {
+        memset(counts, 0, sizeof(counts));
+    }
+    
+    void add(vector<int> point) {
+        int x = point[0];
+        int y = point[1];
+        if (counts[x][y] == 0) {
+            unique_points.push_back({x, y});
+        }
+        counts[x][y]++;
+    }
+    
+    int count(vector<int> point) {
+        int px = point[0];
+        int py = point[1];
+        int total_squares = 0;
+        for (const auto& p : unique_points) {
+            int x = p.first;
+            int y = p.second;
+            if (x == px || y == py) continue;
+            if (abs(x - px) != abs(y - py)) continue;
+            total_squares += counts[x][y] * counts[px][y] * counts[x][py];
+        }
+        
+        return total_squares;
+    }
+};
