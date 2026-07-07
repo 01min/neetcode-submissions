e3a6e9318d58323a9b8a84c170993acf8096fed5
01min <thisis01min@gmail.com>
2026-07-07 14:52:19 +0800

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b != 0) {
+            int sum_without_carry = a ^ b;
+            int carry = (unsigned int)(a & b) << 1;
+            a = sum_without_carry;
+            b = carry;
+        }
+        
+        return a;
+    }
+};
