7575cb975490dc1a342c689e705dee3956a7048d
01min <thisis01min@gmail.com>
2026-07-07 12:44:38 +0800

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total_gas = 0;   
+        int total_cost = 0;  
+        int current_tank = 0; 
+        int start_station = 0; 
+        
+        for (size_t i = 0; i < gas.size(); ++i) {
+            total_gas += gas[i];
+            total_cost += cost[i];
+            current_tank += gas[i] - cost[i];
+            if (current_tank < 0) {
+                start_station = i + 1;
+                current_tank = 0;
+            }
+        }
+        if (total_gas < total_cost) {
+            return -1;
+        }
+        return start_station;
+    }
+};
